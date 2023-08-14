fbc6e6dd36bfd428a0004d4cb712fba74cef3dbc
Lpinheirocl <lucas.pinheiro1014@gmail.com>
2023-08-14 07:08:22 -0300

    subir

--- src/compilado_cli.py
+++ src/compilado_cli.py
@@ -5,3 +5,13 @@ from photoshopy import Photoshopy
 from compilado_feed import CompiladoFeed
 from compilado_formatter import CompiladoFormatter
 import logging
+
+class CompiladoCLI:
+    CLI_VERSION = "Compilado CLI 1.0.0"
+    INI_FILE = "compilado.ini"
+
+    def __load_ini_file(self. filename)
+        config = configparser.ConfigParser()
+        config.read(filename)
+        return config
+    
