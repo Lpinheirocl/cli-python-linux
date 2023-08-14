d89210f8fb68cc9fd4663f876a5a3b07d6bac8ad
Lpinheirocl <lucas.pinheiro1014@gmail.com>
2023-08-14 07:02:07 -0300

    primeira parte

---
+++ src/compilado_cli.py
@@ -0,0 +1,7 @@
+import os, sys
+import argparse
+import configparser
+from photoshopy import Photoshopy
+from compilado_feed import CompiladoFeed
+from compilado_formatter import CompiladoFormatter
+import logging
